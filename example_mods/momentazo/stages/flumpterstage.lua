7d28973ff12300bb7161f492317b020f405892b6
Pbt <pbt@pbtservers.com>
2023-03-12 19:54:44 +0100

    Primera Canción "Momentazo"

---
+++ example_mods/momentazo/stages/flumpterstage.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bgflumpter', 'bgflumpter', -600, -300);
+	setScrollFactor('bgflumpter', 0.9, 0.9);
+
+	addLuaSprite('bgflumpter', false);
+end
