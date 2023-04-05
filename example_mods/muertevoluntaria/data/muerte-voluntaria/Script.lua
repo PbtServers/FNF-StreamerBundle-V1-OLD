a0d8fe15e496530f1f817cd0a5e0b198d380cb56
Pbt <pbt@pbtservers.com>
2023-04-05 15:33:50 +0200

    Casi Terminado, Solo Falta Añadir Detalles Visuales y etc

--- example_mods/muertevoluntaria/data/muerte-voluntaria/Script.lua
+++ example_mods/muertevoluntaria/data/muerte-voluntaria/Script.lua
@@ -2,11 +2,13 @@ function onUpdate()
 
 	setProperty('boyfriend.x', getProperty('boyfriend.x')+5)
 	setProperty('dad.x', getProperty('dad.x')+5)
+	setProperty('gf.x', getProperty('gf.x')+5)
 	
 	if (getProperty('boyfriend.x') >= 9600) then
 	
 		setProperty('boyfriend.x', 600)
 		setProperty('dad.x', 0)
+		setProperty('gf.x', 200)
 	
 	end
 	
