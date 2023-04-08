5c249d0a02a42f4b58658b8bd2b72cd39fde9e80
Pbt <pbt@pbtservers.com>
2023-04-09 00:20:30 +0200

    Nueva Versión del Mod Terminada! [v1.3]

---
+++ example_mods/alertacubica/data/alertacubica/Script.lua
@@ -0,0 +1,30 @@
+function onUpdate()
+
+	if curBeat == 2064 then
+	
+		makeLuaSprite('alerta', 'alerta', -600, -300);
+		setScrollFactor('alerta', 0.9, 0.9);
+
+		addLuaSprite('alerta', true);
+	
+	end
+	
+	if curBeat == 2080 then
+	
+		removeLuaSprite('alerta', false);
+		
+	end
+	
+	if curBeat == 2130 then
+	
+		addLuaSprite('alerta', true);
+	
+	end
+	
+	if curBeat == 2150 then
+	
+		removeLuaSprite('alerta', false);
+		
+	end
+	
+end
